16524d54c9f0efa2ca917f311407daaae9af108e
Trong An <troandev@gmail.com>
2024-06-16 09:14:58 +0700

    push

--- demngay.js
+++ demngay.js
@@ -1,29 +1,30 @@
 $(document).ready(function () {
     var demngay1 = setInterval(function () {
-        CountDownDay("22/01/2024", ".text-countera")
-        CountDownDay("24/12/2023", ".text-counterb")
-        CountDownDay("24/12/2023", ".text-counter-exten")
-        CountDownDay("28/12/2023", ".text-counterd")
-        CountDownDay("22/01/2024", ".text-counter-itest")
-    }, 1000)
-})
+        CountDownDay("22/01/2024", ".text-countera");
+        CountDownDay("24/12/2023", ".text-counterb");
+        CountDownDay("24/12/2023", ".text-counter-exten");
+        CountDownDay("28/12/2023", ".text-counterd");
+        CountDownDay("22/01/2024", ".text-counter-itest");
+        CountDownDay("16/06/2024", ".text-counter-kttt");
+    }, 1000);
+});
 
 function CountDownDay(datetime, classname) {
-    const lastEditDate = moment(datetime, "DD/MM/YYYY")
+    const lastEditDate = moment(datetime, "DD/MM/YYYY");
 
-    const currentDate = moment()
+    const currentDate = moment();
 
-    const day = currentDate.diff(lastEditDate, "days")
+    const day = currentDate.diff(lastEditDate, "days");
     if (day > 365) {
-        var years = Math.floor(day / 365)
-        $(classname).html("Cập nhật " + years + " năm trước")
+        var years = Math.floor(day / 365);
+        $(classname).html("Cập nhật " + years + " năm trước");
     } else if (day > 30) {
-        var months = Math.floor(day / 30)
-        $(classname).html("Cập nhật " + months + " tháng trước")
+        var months = Math.floor(day / 30);
+        $(classname).html("Cập nhật " + months + " tháng trước");
     } else if (day > 7) {
-        var weeks = Math.floor(day / 7)
-        $(classname).html("Cập nhật " + weeks + " tuần trước")
+        var weeks = Math.floor(day / 7);
+        $(classname).html("Cập nhật " + weeks + " tuần trước");
     } else {
-        $(classname).html("Cập nhật " + day + " ngày trước")
+        $(classname).html("Cập nhật " + day + " ngày trước");
     }
 }
